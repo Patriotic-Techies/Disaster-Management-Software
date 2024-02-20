e3429d8c474e7e48725c2b404daf11526e08e134
DivyaSivalingam <143534620+DivyaSivalingam@users.noreply.github.com>
2024-02-20 16:45:53 +0530

    Delete chat directory

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignInView, SignUpView, Team
-
-urlpatterns = [
-    path('signin/', SignInView.as_view()),
-    path('signup/', SignUpView.as_view()),
-    path('team/', Team.as_view()),
-]
